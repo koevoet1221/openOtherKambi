5109f9b617a6590391d98adbba6a500373c69c7e
Koevoet <76625701+koevoet1221@users.noreply.github.com>
2024-03-15 00:40:02 +0100

    add files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # openOtherKambi
 An extention to open another betting site with the same match by a shortcut
+
+# Gebruik
+Installeer de extentie.
+Wanneer je een wedstrijd open hebt op unibet of betcity druk op CTRL + SHIFT + X om de andere site te openen op de juiste wedstrijd.
---
+++ background.js
@@ -0,0 +1,17 @@
+chrome.commands.onCommand.addListener(function(command) {
+  if (command === "openNewTab") {
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+      function onlyNumbers(str) {
+        return /^\d+$/.test(str);
+      }
+      var currentUrl = tabs[0].url;
+      var eventCode = currentUrl.substring(currentUrl.lastIndexOf('/') + 1);
+      if (currentUrl.includes("unibet") && onlyNumbers(eventCode)) {
+        var newUrl = "https://www.betcity.nl/sportsbook#event/" + eventCode;
+      } else if (currentUrl.includes("betcity") && onlyNumbers(eventCode)){
+        var newUrl = "https://www.unibet.nl/betting/sports/event/" + eventCode;
+      }
+      chrome.tabs.create({ url: newUrl, active: false });
+    });
+  }
+});
